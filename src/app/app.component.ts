f595305d836832cb86a020a587852f7d7ba32181
mohamad <mh711748@gmail.com>
2025-03-28 18:02:39 +0330

    ngIf - directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   imports: [
+    CommonModule,
     FormsModule
   ],
   templateUrl: './app.component.html',
@@ -11,6 +13,8 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class AppComponent {
+
+  isShowServers: boolean= false;
 
   serverId: number = 1;
 
@@ -34,15 +38,9 @@ export class AppComponent {
   }
 
 
-  clickFunctionEvent(event: KeyboardEvent) {
-    let value = (event.target as HTMLInputElement).value;
-
-    if (value.length > 0) {
-      this.isAllowButton = false
-    } else {
-      this.isAllowButton = true
-    }
-
+  clickFunctionEvent(event: any) {
+   
+    this.isShowServers = true
 
   }
 
