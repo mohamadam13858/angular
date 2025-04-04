bb5ef4db5a15db4c34c1be7359f517b2b2f2d840
mohamad <mh711748@gmail.com>
2025-04-04 17:22:32 +0330

    Directive-me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicHighlihgtDirective } from './basic-highlight/basic-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlihgtDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+
+@Directive({
+    selector: '[appBasicHighlight]',
+    standalone: false
+})
+export class BasicHighlihgtDirective implements OnInit {
+
+    constructor(private element: ElementRef) {
+    }
+
+
+    ngOnInit() {
+        this.element.nativeElement.style.backgroundColor = 'green'
+    }
+
+
+
+}
