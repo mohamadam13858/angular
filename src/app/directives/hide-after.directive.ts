145ddcfb4a8e22e304bcee7b9473ae61e1f1e2e4
mohamad <mh711748@gmail.com>
2025-04-04 22:16:17 +0330

    TemplateRef-ViewContainerRef

--- src/app/directives/hide-after.directive.ts
+++ src/app/directives/hide-after.directive.ts
@@ -1,11 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[hideAfter]',
   standalone: false
 })
-export class HideAfterDirective {
+export class HideAfterDirective implements OnInit {
+      
 
-  constructor() { }
+
+  @Input() delay = 5000
+
+
+
+  constructor(
+    private tmplate: TemplateRef<any>,
+    private containar: ViewContainerRef) {}
+
+    ngOnInit(): void {
+      this.containar.createEmbeddedView(this.tmplate)
+
+      setTimeout(()=>{
+        this.containar.clear();
+      }, this.delay)
+    }
 
 }
