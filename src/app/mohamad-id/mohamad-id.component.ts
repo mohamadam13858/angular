da7cc2d37301a64fd37dbb7f4083534230bec478
mohamad <mh711748@gmail.com>
2025-04-05 22:42:14 +0330

    subscribe-paramMap ActivatedT

--- src/app/mohamad-id/mohamad-id.component.ts
+++ src/app/mohamad-id/mohamad-id.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-mohamad-id',
@@ -6,6 +7,40 @@ import { Component } from '@angular/core';
   templateUrl: './mohamad-id.component.html',
   styleUrl: './mohamad-id.component.css'
 })
-export class MohamadIdComponent {
+export class MohamadIdComponent implements OnInit {
 
+
+  constructor(private router: ActivatedRoute, private route: Router) {
+
+  }
+
+  public MohamadId: number = 0
+
+  ngOnInit(): void {
+    // let id = parseInt(this.router.snapshot.paramMap.get('id')!)
+    // this.MohamadId = id
+    this.router.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id')!)
+      this.MohamadId = id
+    })
+
+  }
+
+  goNext() {
+    if (this.MohamadId === 5) {
+      this.MohamadId = 1
+    } else {
+      let nextId = this.MohamadId + 1
+      this.route.navigate(['/mohamad', nextId])
+    }
+  }
+
+  goPrevious() {
+    if (this.MohamadId === 1) {
+      this.MohamadId = 5
+    } else {
+      let previousId = this.MohamadId - 1
+      this.route.navigate(['/mohamad', previousId])
+    }
+  }
 }
