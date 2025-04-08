f2f16d309552ba162afa7db808dae310f79c0449
mohamad <mh711748@gmail.com>
2025-04-08 23:51:29 +0330

    form-group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,13 +38,19 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+
+
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+
       gender: new FormControl('male')
+
     })
   }
-    
-  
+
+
 
   onSubmit() {
     console.log(this.signupForm);
