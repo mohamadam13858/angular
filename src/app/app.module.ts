81f67b87eb0aee7b0b7c02b4477a995dd4d1297b
mohamad <mh711748@gmail.com>
2025-04-09 21:49:36 +0330

    CustomPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ShortenPipe } from './shorten.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
