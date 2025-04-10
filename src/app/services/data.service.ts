90637e7ea3c5342a10693ab995058d15b0b27dbe
mohamad <mh711748@gmail.com>
2025-04-10 19:01:12 +0330

    dataServece

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,59 @@
+import { inject, Injectable } from '@angular/core';
+import { Posts } from '../app.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AppError } from '../errors/app-error';
+import { BadInput } from '../errors/badinput-error';
+import { NotFoundError } from '../errors/not-found-error';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  posts: Posts[];
+
+  constructor(private url : string , private http: HttpClient) { }
+
+
+  create(res: any) {
+    return this.http.post(this.url, JSON.stringify(res)).pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  get() {
+    return this.http.get<Array<Posts>>(this.url).pipe(catchError(this.errorHandler))
+  }
+
+  update(res: any) {
+    return this.http.put(this.url + '/' + res.id, JSON.stringify(res), {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+  delete(res: any) {
+    return this.http.delete(this.url + '/' + res.id).pipe(
+      catchError(this.errorHandler)
+    )
+  }
+
+
+
+
+
+  private errorHandler(err: any) {
+    if (err.status === 400) {
+      return throwError(() => new BadInput(err))
+    }
+    if (err.status === 404) {
+      return throwError(() => new NotFoundError(err))
+    }
+    return throwError(() => {
+      new AppError(err)
+    })
+  }
+}
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,58 +5,21 @@ import { catchError, throwError } from 'rxjs';
 import { AppError } from '../errors/app-error';
 import { NotFoundError } from '../errors/not-found-error';
 import { BadInput } from '../errors/badinput-error';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PostService {
-  url: string = 'https://jsonplaceholder.typicode.com/posts'
-  posts: Posts[];
-
-  constructor(private http: HttpClient) { }
+export class PostService extends DataService {
+  // url: string = 'https://jsonplaceholder.typicode.com/posts'
 
 
-  create(post: any) {
-    return this.http.post(this.url, JSON.stringify(post)).pipe(
-      catchError(this.errorHandler)
-    )
-  }
+  constructor( http: HttpClient ) { 
 
-  get() {
-    return this.http.get<Array<Posts>>(this.url).pipe(catchError(this.errorHandler))
-  }
-
-  update(post: any) {
-    return this.http.put(this.url + '/' + post.id, JSON.stringify(post), {
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    }).pipe(
-      catchError(this.errorHandler)
-    )
-  }
-
-  delete(post: any) {
-    return this.http.delete(this.url + '/' + post.id).pipe(
-      catchError(this.errorHandler)
-    )
+    super('https://jsonplaceholder.typicode.com/posts' , http)
   }
 
 
-
-
-
-  private errorHandler(err: any) {
-    if (err.status === 400) {
-      return throwError(() => new BadInput(err))
-    }
-    if (err.status === 404) {
-      return throwError(() => new NotFoundError(err))
-    }
-    return throwError(() => {
-      new AppError(err)
-    })
-  }
 
 
 
